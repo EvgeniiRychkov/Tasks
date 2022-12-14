8c3600044ae3f7642c85785e3b9c0664418b93cf
EvgeniiRychkov <richkovevgeniy@gmail.com>
2022-12-13 18:30:57 -0800

    [6] add ability to complete a task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+	scope :finished,-> { where(finished: true) }	
+	scope :unfinished,-> { where(finished: false) }	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tasks, except: :show
+  resources :tasks, except: :show do
+    member do
+      post :finish
+    end
+  end
 
   root 'tasks#index'
 end
